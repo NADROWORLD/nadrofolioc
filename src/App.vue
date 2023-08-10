4eb41e5ea0fee3de1731bf33d469c51f17bf1e85
Jordan <jordan@MacBook-Pro-de-Jordan.local>
2023-08-10 16:03:00 +0200

    v1

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <header>
+    <nav class="">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-8"
+      >
+        <a href="#" class="flex items-center">
+          <div class="logo">
+            <span class="self-center text-[30px] space-nowrap text-[#121212]"
+              >JORDAN BRUNEEL.</span
+            >
+          </div>
+        </a>
+
+        <div class="flex md:order-2">
+          <button
+            type="button"
+            class="focus:ring-4 focus:outline-none font-medium rounded-lg text-[17px] px-4 py-2 text-center mr-3 md:mr-0"
+            id="btn-nav"
+          >
+            <div class="space-y-2">
+              <span class="block w-7 h-0.5 bg-[#121212]"></span>
+              <span class="block w-7 h-0.5 bg-[#121212]"></span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div
+    class="mt-[195px] max-w-screen-xl flex flex-wrap items-center justify-between mx-auto"
+  >
+    <div class="w-[1230px] h-[1930px] relative">
+      <div class="w-[1230px] h-[400px] left-0 top-0 absolute">
+        <div
+          class="w-[800px] h-[400px] px-[29px] left-0 top-0 absolute bg-gray-50 rounded-[60px] justify-center items-center inline-flex"
+        >
+          <div class="flex-col justify-center items-center inline-flex">
+            <img class="w-[250px] h-[250px]" src="./assets/images/moi.png" />
+            <div
+              class="pl-[23px] pr-6 pt-[11px] pb-3 bg-black rounded-[30px] justify-center items-center inline-flex"
+            >
+              <div class="text-white text-[15px] font-normal">Change me</div>
+            </div>
+          </div>
+          <div class="flex-col justify-start items-center gap-5 inline-flex">
+            <div id="info" class="w-[495px] text-black text-[40px] font-normal">
+              FRONT-END DEVELOPPER <br />& UX/UI DESIGNER
+            </div>
+            <div id="info2" class="self-stretch text-black text-xl font-light">
+              Hello , i'm Jordan, a junior front-end developper & <br />UX/UI
+              designer based in Paris ✨
+            </div>
+          </div>
+        </div>
+        <div
+          class="w-[400px] h-[400px] px-[29px] left-[830px] top-0 absolute bg-[url('../assets/images/154.png')] rounded-[60px] justify-center items-center inline-flex"
+        >
+          <div
+            id="info"
+            class="text-white leading-trim leading-normal text-shadow text-[100px] font-normal"
+          >
+            SKILLS
+          </div>
+        </div>
+      </div>
+      <div
+        class="w-[800px] h-[255px] left-[430px] top-[430px] absolute bg-gray-50 rounded-[60px]"
+      ></div>
+      <div
+        class="w-[400px] h-[670px] left-0 top-[430px] absolute bg-gray-50 rounded-[60px]"
+      >
+        <div
+          class="w-[35px] h-[34.12px] left-[32px] top-[591px] absolute"
+        ></div>
+        <div
+          class="w-[202.58px] h-[410.41px] left-[100.58px] top-[128.84px] absolute flex-col justify-start items-start flex"
+        ></div>
+      </div>
+
+      <div
+        class="w-[400px] h-[800px] left-[830px] top-[1130px] absolute bg-gray-50 rounded-[60px]"
+      ></div>
+      <div
+        class="w-[800px] h-[800px] left-0 top-[1130px] absolute bg-gray-50 rounded-[60px]"
+      ></div>
+      <div
+        class="w-[385px] h-[385px] left-[430px] top-[715px] absolute bg-gray-50 rounded-[60px]"
+      ></div>
+      <div
+        class="w-[385px] h-[385px] left-[845px] top-[715px] absolute bg-gray-50 rounded-[60px]"
+      ></div>
+    </div>
+  </div>
+</template>
